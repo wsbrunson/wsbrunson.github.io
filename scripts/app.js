e4e7a41ecad6adcb5fb17d0910a1ec77c7132592
Shane Brunson <brunson.ws@gmail.com>
2014-11-24 17:48:16 -0500

    added squareClass to function addSquare

--- scripts/app.js
+++ scripts/app.js
@@ -6,18 +6,19 @@ var dayObject = function (completed, date) {
 var addSquare = function() {
   var numberOfSquares = 365;
   for(var i = 0; i < numberOfSquares; i++) {
-      var randomNumber = Math.random() * 6
-
+      var randomNumber = Math.random() * 6,
+          squareClass = " sq" + i
+      
       if (randomNumber < 2) {
-        var message = ('<canvas class="square green"></canvas>');
+        var message = ('<canvas class="square green' + squareClass + '"></canvas>');
       }
 
       else if (randomNumber > 4) {
-        var message = ('<canvas class="square red"></canvas>');
+        var message = ('<canvas class="square red' + squareClass + '"></canvas>');
       }
 
       else {
-        var message = ('<canvas class="square gray"></canvas>');
+        var message = ('<canvas class="square gray' + squareClass + '"></canvas>');
       }
 
       $('.main').find("div").append(message);
