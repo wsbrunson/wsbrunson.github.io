b51acb037083a2e2f38468e23ec4955bfd6e00b5
Shane Brunson <brunson.ws@gmail.com>
2014-12-08 22:57:42 -0500

    added object support to addSquare
    addSquare now takes an array of objects

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,59 @@
-var addSquare = function() {
+var habitSquare = function(completed) {
+  this.completed = completed;
+
+  this.colorSquare = function() {
+    var squareHTML;
+
+    if (this.completed) {
+      squareHTML = ('<div class="square green"></div>');
+    }
+
+    else {
+      squareHTML = ('<div class="square red"></div>');
+    }
+
+    return squareHTML;
+  };
+
+  this.htmlClass = this.colorSquare();
+
+};
+
+var newSquare = new habitSquare(true);
+console.log(newSquare);
+console.log(newSquare.htmlClass);
+
+console.log(new habitSquare(true));
+
+var newHabitUser = [];
+newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new habitSquare(false));
+newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new habitSquare(false));
+newHabitUser.push(new habitSquare(true));
+
+console.log(newHabitUser.length);
+
+var addSquare = function(squares) {
   var numberOfSquares = 365;
-  for(var i = 0; i < numberOfSquares; i++) {
-      var randomNumber = Math.random() * 6,
-          squareClass = " sq" + (i + 1)
+  var squareObjects = squares.length;
+  var graySquares = numberOfSquares - squareObjects;
+  console.log(graySquares);
 
-      if (randomNumber < 2) {
-        var message = ('<div class="square green' + squareClass + '"></div>');
-      }
+  for(var i = 0; i < squareObjects; i++) {
+    $('.main').append(squares[i].htmlClass);
+    console.log(squares[i].htmlClass);
+  }
 
-      else if (randomNumber > 4) {
-        var message = ('<div class="square red' + squareClass + '"></div>');
-      }
-
-      else {
-        var message = ('<div class="square gray' + squareClass + '"></div>');
-      }
-
+  if (graySquares) {
+    for(var i = 0; i < graySquares; i++) {
+      message = ('<div class="square gray"></div>');
       $('.main').append(message);
+    }
   }
 };
 
-$(document).ready(addSquare());
+$(document).ready(addSquare(newHabitUser));
