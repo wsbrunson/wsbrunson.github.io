8a555550726aa59bd46541a4697c6c5cded18d6f
Shane Brunson <brunson.ws@gmail.com>
2014-12-11 15:10:48 -0500

    add user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
-var habitSquare = function(completed) {
+var HabitSquare = function(completed) {
   this.completed = completed;
 
   this.colorSquare = function() {
     var squareHTML,
         greenSquare = '<div class="square green"></div>',
         redSquare =  '<div class="square red"></div>';
-        
+
     squareHTML = this.completed ? greenSquare : redSquare;
     return squareHTML;
   };
@@ -14,33 +14,39 @@ var habitSquare = function(completed) {
 
 };
 
-var newSquare = new habitSquare(true);
-console.log(newSquare);
-console.log(newSquare.htmlClass);
-
-console.log(new habitSquare(true));
-
 var newHabitUser = [];
-newHabitUser.push(new habitSquare(true));
-newHabitUser.push(new habitSquare(true));
-newHabitUser.push(new habitSquare(false));
-newHabitUser.push(new habitSquare(true));
-newHabitUser.push(new habitSquare(true));
-newHabitUser.push(new habitSquare(true));
-newHabitUser.push(new habitSquare(false));
-newHabitUser.push(new habitSquare(true));
+newHabitUser.push(new HabitSquare(true, "thr Nov 27 2014"));
+newHabitUser.push(new HabitSquare(true, "fri Nov 28 2014"));
+newHabitUser.push(new HabitSquare(false, "sat Nov 29 2014"));
+newHabitUser.push(new HabitSquare(true, "sun Nov 30 2014"));
+newHabitUser.push(new HabitSquare(true, "mon Dec 01 2014"));
+newHabitUser.push(new HabitSquare(true, "tue Dec 02 2014"));
+newHabitUser.push(new HabitSquare(false, "wed Dec 03 2014"));
+newHabitUser.push(new HabitSquare(true, "thr Dec 04 2014"));
+newHabitUser.push(new HabitSquare(true, "fri Dec 05 2014"));
+newHabitUser.push(new HabitSquare(true, "sat Dec 06 2014"));
+newHabitUser.push(new HabitSquare(true, "sun Dec 07 2014"));
+newHabitUser.push(new HabitSquare(true, "mon Dec 08 2014"));
+newHabitUser.push(new HabitSquare(true, "tue Dec 09 2014"));
+newHabitUser.push(new HabitSquare(false));
 
-console.log(newHabitUser.length);
+console.log(newHabitUser);
+
+var squareLookup = function() {
+  var lookup = {};
+  for (var i = 0, len = array.length; i < len; i++) {
+    lookup[array[i].id] = array[i];
+}
+
+};
 
 var addSquare = function(squares) {
-  var numberOfSquares = 365;
+  var numberOfSquares = 364;
   var squareObjects = squares.length;
   var graySquares = numberOfSquares - squareObjects;
-  console.log(graySquares);
 
   for(var i = 0; i < squareObjects; i++) {
     $('.main').append(squares[i].htmlClass);
-    console.log(squares[i].htmlClass);
   }
 
   if (graySquares) {
@@ -51,4 +57,19 @@ var addSquare = function(squares) {
   }
 };
 
-$(document).ready(addSquare(newHabitUser));
+var removeSquares = function() {
+  $('.main *').remove();
+};
+
+$(document).ready(function() {
+  addSquare(newHabitUser);
+
+  $('input').click(function() {
+    var dateOnClick = new Date();
+    console.log(dateOnClick);
+    console.log(dateOnClick.toDateString());
+    newHabitUser.push(new HabitSquare(true));
+    removeSquares();
+    addSquare(newHabitUser);
+  });
+});
