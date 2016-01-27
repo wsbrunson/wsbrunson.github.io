225083f45b9a57427ea4bb43faba1f5d8b87b043
wsbrunson <brunson.ws@gmail.com>
2016-01-26 21:17:31 -0500

    blog is up

--- js/header.js
+++ js/header.js
@@ -1,14 +1,19 @@
 (function(document) {
 
 	var currentActiveNavElement = document.getElementsByClassName('global-header-nav-item isActive');
+	var navItems = document.getElementsByClassName('global-header-nav-item');
+	var pageHeading = document.getElementsByTagName('h2');
+	var pageHeadingName = pageHeading[0].getAttribute('data-nav');
 
 	if (currentActiveNavElement.length > 0) {
 		currentActiveNavElement[0].classList.remove('isActive');
 	}
 
-	//navElement[0].addEventListener('click', addActiveClassToNavItems);
+	navItems = [].filter.call(navItems, function(item) {
+		var headerNavText = item.getElementsByTagName('a')[0].textContent.toLowerCase();
+		return headerNavText === pageHeadingName;
+	});
 
-	//function addActiveClassToNavItems() {
-		//console.log(this);
-	//}
+	navItems[0].classList.add('isActive');
+
 })(document);
